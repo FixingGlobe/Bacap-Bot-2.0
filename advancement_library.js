eaf06543bc509ed215b25655ce9eb9e5ef8e7d6c
FixingGlobe <89920712+FixingGlobe@users.noreply.github.com>
2024-02-08 19:34:46 +0100

    Create advancement_library.js
    + added dummy

---
+++ advancement_library.js
@@ -0,0 +1,20 @@
+// a big file with JS objects for each advancement
+
+//
+
+var iHateYou = "var … A devs biggest nightmare!";
+
+let happy10yearsMcButAsAnImposter = {
+  adv_name: "Happy 10 years MC! Will be included as Well :barry:",
+  legacy: true,  // will color it
+  tab: "statistics",
+  type: "goal",
+  description: "YOU'RE MOTHER!",
+  xp: "It's over 9000",
+  reward: "10 potatos",
+  trophy: "potato salad",
+  hardcore: "play for 10 years without dying",
+  expansion: "In terralith you need to drink Royal Jelly as well.",
+  legacy_description: "Only obtainable in BACAP 1.7 but this is an imposter",
+};
+
