d52ebc9945781694fec1f0c90ecd176ef3caa9ba
FixingGlobe <89920712+FixingGlobe@users.noreply.github.com>
2024-02-08 19:49:40 +0100

    Update advancement_library.js

--- advancement_library.js
+++ advancement_library.js
@@ -7,6 +7,7 @@ var iHateYou = "var … A devs biggest nightmare!";
 let happy10yearsMcButAsAnImposter = {
   adv_name: "Happy 10 years MC! Will be included as Well :barry:",
   legacy: true,    // will do a text format like italic
+  hardcore: true,  // will make it bold
   expansion: true,  // will color it
   tab: "statistics",
   type: "goal",
@@ -14,8 +15,8 @@ let happy10yearsMcButAsAnImposter = {
   xp: "It's over 9000",
   reward: "10 potatos",
   trophy: "potato salad",
-  hardcore: "play for 10 years without dying",
-  expansion_description: "In terralith you need to drink Royal Jelly as well.",
   legacy_description: "Only obtainable in BACAP 1.7 but this is an imposter",
+  hardcore_description: "play for 10 years without dying",
+  expansion_description: "In terralith you need to drink Royal Jelly as well.",
 };
 
