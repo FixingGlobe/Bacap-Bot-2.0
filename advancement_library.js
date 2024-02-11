3240d5362be16499816c6e0786131156464697e7
FixingGlobe <89920712+FixingGlobe@users.noreply.github.com>
2024-02-11 08:43:47 +0100

    Update advancement_library.js

--- advancement_library.js
+++ advancement_library.js
@@ -5,7 +5,7 @@
 var iHateYou = "var … A dev's worst nightmare!";
 
 let happy10yearsMcButAsAnImposter = {  // perhaps I should name it the adv[id] instead of a name
-  adv_id: 0,    // I'm assigning these randomly except 1000, also id 0 is a test object  // ids are for dbs
+  id: 0,    // I'm assigning these randomly except 1000, also id 0 is a test object  // ids are for dbs
   adv_name: "Happy 10 years MC! Will be included as Well :barry:",
   hidden: true,  // will emphasize (italic)
   legacy: true,    // will add a spoiler to the advancement name
